8d59db7a0322534df44bff381953cc9cb25ef47d
wolfstrop <witewolf32@gmail.com>
2026-04-08 01:59:23 -0600

    base misiones

---
+++ app/rutas/missiones.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+ms_bl = Blueprint('misiones', __name__, url_prefix='/misiones')
+
+@ms_bl.route('/misiones', methods=['POST'])
+
+#agregar mision
+
+#lista de misiones
+
+
+
